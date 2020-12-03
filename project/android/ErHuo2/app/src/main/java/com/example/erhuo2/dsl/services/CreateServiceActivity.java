3b7a99afda81780a3961a7c2379d69aa5eda7fe5
e <2498354874@qq.com>
2020-12-03 22:26:56 +0800

    dsl

--- project/android/ErHuo2/app/src/main/java/com/example/erhuo2/dsl/services/CreateServiceActivity.java
+++ project/android/ErHuo2/app/src/main/java/com/example/erhuo2/dsl/services/CreateServiceActivity.java
@@ -1,31 +1,30 @@
 package com.example.erhuo2.dsl.services;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.net.Uri;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.example.erhuo2.R;
+import com.example.erhuo2.dsl.services.view.SquareImageView;
 import com.yanzhenjie.album.Action;
 import com.yanzhenjie.album.Album;
 import com.yanzhenjie.album.AlbumConfig;
 import com.yanzhenjie.album.AlbumFile;
 import com.yanzhenjie.album.AlbumLoader;
 
-
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,7 +35,9 @@ public class CreateServiceActivity extends AppCompatActivity {
     private ImageView create_service_getimg;
     private RelativeLayout border_getimg;
     private Button create_service_submit;
-    private ArrayList<AlbumFile> mAlbumFiles;
+    private ArrayList<AlbumFile> mAlbumFiles = new ArrayList<>();
+    private List<Bitmap> bitmaps = new ArrayList<>();
+    private GridLayout create_service_imgs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +62,7 @@ public class CreateServiceActivity extends AppCompatActivity {
         create_service_getimg = findViewById(R.id.create_service_getimg);
         border_getimg = findViewById(R.id.border_getimg);
         create_service_submit = findViewById(R.id.create_service_submit);
+        create_service_imgs = findViewById(R.id.create_service_imgs);
 
     }
 
@@ -93,19 +95,40 @@ public class CreateServiceActivity extends AppCompatActivity {
                 .selectCount(6)
                 .checkedList(mAlbumFiles)
                 .onResult(new Action<ArrayList<AlbumFile>>() {
+                    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
                     @Override
                     public void onAction(@NonNull ArrayList<AlbumFile> result) {
-                        Log.e("dsl",result.size()+"");
+                        create_service_imgs.removeAllViews();
+                        bitmaps.clear();
+                        if(result.size()>0){
+                            create_service_imgs.setVisibility(View.VISIBLE);
+                            for(int i = 0; i < result.size(); i++){
+                                Bitmap bit = BitmapFactory.decodeFile((result.get(i).getPath()));
+                                bitmaps.add(bit);
+
+                                GridLayout.Spec rowSpec = GridLayout.spec(i/3);//行数
+                                GridLayout.Spec columnSpec = GridLayout.spec(i%3, 1.0f);//列数 列宽的比例 weight=1
+                                ImageView imageView = new SquareImageView(getApplicationContext());
+                                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                                //由于宽（即列）已经定义权重比例 宽设置为0 保证均分
+                                GridLayout.LayoutParams layoutParams = new GridLayout.LayoutParams(new ViewGroup.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT));
+                                layoutParams.rowSpec = rowSpec;
+                                layoutParams.columnSpec = columnSpec;
+
+                                layoutParams.setMargins(5, 5, 5, 5);
+
+                                imageView.setImageBitmap(bit);
+                                create_service_imgs.addView(imageView, layoutParams);
+                            }
+                        }
                     }
                 })
                 .onCancel(new Action<String>() {
                     @Override
                     public void onAction(@NonNull String result) {
-                        Log.e("dsl","222");
                     }
                 })
                 .start();
-
 
     }
 
@@ -118,7 +141,6 @@ public class CreateServiceActivity extends AppCompatActivity {
 
         @Override
         public void load(ImageView imageView, String url) {
-            Log.e("dsl",url);
             Glide.with(imageView.getContext())
                     .load(url)
                     .error(R.drawable.write)
